edff1bc8cfc188110c8c776063f3e9edfd90c02c
Abdulla <abdulla.binissa.176@gmail.com>
2021-02-01 21:57:06 -0800

    Interactable Cells
    Cells clicked on will cycle red hues

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import Data
 
 WIDTH, HEIGHT = 1200, 800 #1200
 SQ_SIZE =  16 
-MAX_FPS = 1
+MAX_FPS = 15
 
 def main():
     pygame.init()
@@ -14,6 +14,9 @@ def main():
 
     pygame.display.set_caption('Jobs to Houses')
     screen.fill(pygame.Color('white'))
+    screen.set_alpha(128)
+
+    state = Data.State()
 
     mainLoop = True
     while mainLoop:
@@ -26,21 +29,24 @@ def main():
                 location = pygame.mouse.get_pos()
                 col = location[0] // SQ_SIZE
                 row = location[1] // SQ_SIZE
-                squareSelected = (row, col)
+                cellSelected = (row, col)
 
-                #state.setSquare(squareSelected)
+                state.setCell(cellSelected)
 
-        drawState(screen)
+            
+
+        drawGrid(screen)
+        drawCells(screen, state)
+
         clock.tick(MAX_FPS)
         pygame.display.flip()
 
     
-def drawState(screen):
+def drawGrid(screen):
     for r in range( HEIGHT // SQ_SIZE  ):
         for c in range( WIDTH // SQ_SIZE ):
-            padding = 0.5
-            margin = 4
-            color = int( 255 - (0.2 * 255) )
+
+            padding, margin = 0.5, 4
             squareOuter = pygame.Rect(
                 c*SQ_SIZE + padding, 
                 r*SQ_SIZE + padding, 
@@ -52,14 +58,51 @@ def drawState(screen):
                 SQ_SIZE - 2*padding - 2*margin, 
                 SQ_SIZE - 2*padding - 2*margin)
 
-            pygame.draw.rect(screen, getSquareColor(0), squareOuter)
+            pygame.draw.rect(screen, pygame.Color(200, 200, 200), squareOuter)
             pygame.draw.rect(screen, pygame.Color( "white"), squareInner)
 
-def getSquareColor(num):
 
-    gray = int( 255 - (0.2 * 255) )
+def drawCells(screen, state):
+    surface = pygame.Surface( (HEIGHT, WIDTH) )
+    surface.fill('white')
+    surface.set_alpha(0)
     
-    if num == 0:
-        return pygame.Color(gray, gray, gray)
+    for r in range( HEIGHT // SQ_SIZE  ):
+        for c in range( WIDTH // SQ_SIZE ):
+            cellValue = state.getCellValue((r, c))
+            cell = pygame.Surface( (SQ_SIZE, SQ_SIZE) )
+            cell.fill('white')
+
+            if cellValue < 0:
+                cellColor = pygame.Color(255, 0, 0) 
+                alpha = int( abs(cellValue)*255 )
+            elif cellValue > 0:
+                cellColor = pygame.Color(11, 156, 49)
+                alpha = int( abs(cellValue)*255 )
+            else:
+                cellColor = pygame.Color(200, 200, 200)
+                alpha = 0
+                
+            cell.set_alpha( alpha )
+
+            padding, margin = 0.5, 4
+            squareOuter = pygame.Rect(
+                padding, 
+                padding, 
+                SQ_SIZE - 2*padding, 
+                SQ_SIZE - 2*padding)
+            squareInner = pygame.Rect(
+                padding + margin, 
+                padding + margin, 
+                SQ_SIZE - 2*padding - 2*margin, 
+                SQ_SIZE - 2*padding - 2*margin)
+
+            pygame.draw.rect(cell, cellColor, squareOuter)
+            pygame.draw.rect(cell, pygame.Color( "white"), squareInner)
+            screen.blit(cell, (c*SQ_SIZE,r*SQ_SIZE)) 
+
+    #screen.blit(surface, (0,0))
+
+            
 
 main()
